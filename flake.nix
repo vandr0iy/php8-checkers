c84447219f5bb4bcd56554422333ecae8814d1d9
Vandr0iy <5510334+vandr0iy@users.noreply.github.com>
2022-09-29 01:05:53 +0200

    updating flake + composer

--- flake.nix
+++ flake.nix
@@ -1,24 +1,55 @@
 {
   description = "Nix flake";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell = {
+      url = "github:numtide/devshell/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+  outputs = {
+    self,
+    devshell,
+    nixpkgs,
+    flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config = { allowUnfree = true; };
+          overlays = [ devshell.overlay ];
         };
       in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            just
-            php
-            php81packages.composer
+        devShell = pkgs.devshell.mkShell {
+          name = "PHP8-dev-shell";
+          commands = [
+            {
+              name = "unittest";
+              category = "repl";
+              help = "Run unit tests with phpunit";
+              command = "./vendor/bin/phpunit";
+            }
+            {
+              name = "cinst";
+              category = "repl";
+              help = "short for 'composer install'";
+              command = "composer install";
+            }
+            {
+              name = "cupd";
+              category = "repl";
+              help = "short for 'composer update'";
+              command = "composer update";
+            }
+          ];
+          packages = with pkgs; [
+            php81
+            php81Packages.composer
           ];
         };
       }
     );
 }
-
